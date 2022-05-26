c7a1a08517f9a4dec3b4fb6484b869ef68503755
HectorCan <habacab@gmail.com>
2022-05-25 19:35:10 -0500

    save

--- Assets/Script/MainPanel.cs
+++ Assets/Script/MainPanel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainPanel : MonoBehaviour
@@ -9,10 +10,15 @@ public class MainPanel : MonoBehaviour
     [Header("Opciones")]
     public Slider volumen;
     public Toggle mute;
+    public AudioMixer mixer;
 
     [Header("Panels")]
     public GameObject mainPanel;
     public GameObject optionsPanel;
+
+    public void Awake(){
+        volumen.onValueChanged.AddListener(ChangedVolMaster);
+    }
 
     public void openPanel(GameObject panel){
         mainPanel.SetActive(false);
@@ -22,6 +28,10 @@ public class MainPanel : MonoBehaviour
 
     public void Jugar(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+    }
+
+    public void ChangedVolMaster(float v){
+        mixer.SetFloat("VolMaster",v);
     }
 
 
