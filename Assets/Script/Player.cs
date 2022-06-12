f631e29a4f77205512bd33090f30690c282d36e2
Jose Francisco Peralta Ramos <49628919+JFrank510@users.noreply.github.com>
2022-06-11 19:39:47 -0500

    Build a system shoot & coalition

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] 
-    private float speed = 100f;
+    public Projectile redPrefab;
+    public float speed = 5.0f;
+    private bool _redActive;
     private void Update()
     {
-        if (Input.GetKey(KeyCode.D)) 
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            this.transform.position += Vector3.left * this.speed * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.A)) 
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            shoot();
         }
     }
+
+    private void shoot(){
+        if(!_redActive){
+
+            Projectile projectile = Instantiate(this.redPrefab, this.transform.position, Quaternion.identity);
+            projectile.destroyed += RedDestroyed;
+            _redActive = true;
+        }
+    }
+
+    private void RedDestroyed(){
+        _redActive = false;
+
+    }
+
+
 }
+
