9fbb497ecdf43ff9fbb6f895fc12247cd6091170
Jose Francisco Peralta Ramos <49628919+JFrank510@users.noreply.github.com>
2022-09-22 02:22:47 -0500

    Panel question(random questions)

--- Assets/Script/QuestionTypes/QuestionGenerate.cs
+++ Assets/Script/QuestionTypes/QuestionGenerate.cs
@@ -7,94 +7,124 @@ public class QuestionGenerate : MonoBehaviour
     private static SpawnerFishesQ spawn;
     public static string currentAnswer, correctAnswer;
     
-    public GameObject QuestionImg;
-    private int A,B,C,D,randomValue;
+    public GameObject QuestionImg,QuestionImg2,QuestionImg3;
+    private int A,B,C,D,randomQuestion,randomAnswer,randomNumber;
 
     private string AF,BF,CF,DF;
 
     private int[] swapAns = new int[4];
 
-    public System.Random random = new System.Random();
+    private List<int> listNumbers = new List<int>();
+
+    private System.Random random = new System.Random();
 
     // Start is called before the first frame update
     void Start()
     {
         spawn = FindObjectOfType<SpawnerFishesQ>();
         spawn.DoSpawn();
-        GenerateAnswers();
         QuestionsView();
-        // Esperar n segundos
-
-        // Mostrar panel de preguntas
-
-        // Remover peces
-
-        // Cuando se resuelva regresa a mostrar peces
-        // spawn.spawnLFQ
-        // Debug.Log(spawn.spawnGFQ);
-        // Debug.Log(spawn.spawnPFQ);
-
-        
     }
 
-    public void GenerateAnswers(){
-        A =  Random.Range(spawn.spawnLFQ + 1, 8);
-        B =  Random.Range(A + 1 , 10);
-        C =  Random.Range(B + 1, 12);
-        D =  Random.Range(D + 1, 14);
+    public void RandomAnswers()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            do {
+                randomNumber = random.Next(1, 5);
+            } while (listNumbers.Contains(randomNumber));
+            listNumbers.Add(randomNumber);
+        }
+    }
+
+
+    public void GenerateAnswers(int spawnType)
+    {
+        RandomAnswers();
+        A = listNumbers[0] + spawnType;
+        B = listNumbers[1] + spawnType;
+        C = listNumbers[2] + spawnType;
+        D = listNumbers[3] + spawnType;
         swapAns = new int[4] { A, B, C, D };
-        randomValue = swapAns[random.Next(0, swapAns.Length)];
-        if(randomValue == A){
-            A = spawn.spawnLFQ;
+        randomAnswer = swapAns[random.Next(0, swapAns.Length)];
+        if(randomAnswer == A){
+            A = spawnType;
         }
 
-        else if(randomValue == B){
-            B = spawn.spawnLFQ;
+        else if(randomAnswer == B){
+            B = spawnType;
         }
 
-        else if(randomValue == C){
-            C = spawn.spawnLFQ;
+        else if(randomAnswer == C){
+            C = spawnType;
         }
 
-        else if(randomValue == D){
-            D = spawn.spawnLFQ;
+        else if(randomAnswer == D){
+            D = spawnType;
         }
         AF = A.ToString();
         BF = B.ToString();
         CF = C.ToString();
         DF = D.ToString();
-        correctAnswer = spawn.spawnLFQ.ToString();
-        //correctAnswer2 = spawn.spawnGFQ.ToString();
-        //correctAnswer3 = spawn.spawnPFQ.ToString();
+        correctAnswer = spawnType.ToString();
     }
 
     public void QuestionsView()
     {
-        QuestionDisplay.newQuestion = "Cuantas veces observaste esta especie";
-        QuestionImg.SetActive(true);
-        QuestionDisplay.newA = AF;
-        QuestionDisplay.newB = BF;
-        QuestionDisplay.newC = CF;
-        QuestionDisplay.newD = DF;
-        CheckAnswers();
+        randomQuestion = Random.Range(1,3);
+        if(randomQuestion == 1)
+        {
+            GenerateAnswers(spawn.spawnLFQ);
+            QuestionDisplay.newQuestion = "Cuantas veces observaste esta especie:";
+            QuestionImg.SetActive(true);
+            QuestionDisplay.newA = AF;
+            QuestionDisplay.newB = BF;
+            QuestionDisplay.newC = CF;
+            QuestionDisplay.newD = DF;
+            CheckAnswers(correctAnswer);
+        }
+
+        if(randomQuestion == 2)
+        {
+            GenerateAnswers(spawn.spawnGFQ);
+            QuestionDisplay.newQuestion = "Cuantas veces observaste esta especie:";
+            QuestionImg2.SetActive(true);
+            QuestionDisplay.newA = AF;
+            QuestionDisplay.newB = BF;
+            QuestionDisplay.newC = CF;
+            QuestionDisplay.newD = DF;
+            CheckAnswers(correctAnswer);
+        }
+
+        if(randomQuestion == 3)
+        {
+            GenerateAnswers(spawn.spawnPFQ);
+            QuestionDisplay.newQuestion = "Cuantas veces observaste esta especie:";
+            QuestionImg3.SetActive(true);
+            QuestionDisplay.newA = AF;
+            QuestionDisplay.newB = BF;
+            QuestionDisplay.newC = CF;
+            QuestionDisplay.newD = DF;
+            CheckAnswers(correctAnswer);
+        }
     }
-    public void CheckAnswers()
+    public void CheckAnswers(string Answer)
     {
         // Debug.Log(correctAnswer);
         // Debug.Log(answer);
-        if (AF == correctAnswer){
+        if (AF == Answer){
             currentAnswer = "A";
         }
 
-        if (BF == correctAnswer){
+        if (BF == Answer){
             currentAnswer = "B";
         }
 
-        if (CF == correctAnswer){
+        if (CF == Answer){
             currentAnswer = "C";
         }
 
-        if (DF == correctAnswer){
+        if (DF == Answer){
             currentAnswer = "D";
         }
         
